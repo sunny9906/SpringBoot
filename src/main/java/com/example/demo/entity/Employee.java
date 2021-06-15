054ebf0b79e5d2c76a353cd551fc5ef9561e7c1d
sanketh <sankethsunny999@gmail.com>
2021-06-15 15:50:13 +0530

    Employee class

--- src/main/java/com/example/demo/entity/Employee.java
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -1,10 +1,19 @@
 package com.example.demo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name="emp")
+@Getter
+@Setter
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private  String name;
 
 }
